730854b6bf2c92da3a556aa815e85143bda74f0c
YRCV <yrc@njit.edu>
2026-03-24 22:10:55 -0400

    feat: add footer with legal disclaimer

--- arduino-week-docs/.vitepress/config.js
+++ arduino-week-docs/.vitepress/config.js
@@ -39,6 +39,11 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/YRCV/arduino-week' }
-    ]
+    ],
+
+    footer: {
+      message: 'Presented by IEEE NJIT Student Branch<br>Documentation by Yahil<br><span style="display: inline-block; margin-top: 8px; max-width: 500px; font-size: 0.85em; opacity: 0.8; line-height: 1.5; text-align: center;">Arduino® is a registered trademark of Arduino SA. This event is organized by the IEEE NJIT Student Branch and funded by the NJIT Student Senate. It does not represent, nor is it officially endorsed by, Arduino SA or the global IEEE organization.</span>',
+      copyright: 'Copyright © 2026 IEEE NJIT Student Branch'
+    }
   }
 })
